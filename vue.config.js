a552f3d9bc3a3a96f6a06f57c60c5b5d888c5638
冯尧 <fengyao@glaway.com>
2020-07-06 16:17:28 +0800

    图标修改为字体库；优化登录页面

--- vue.config.js
+++ vue.config.js
@@ -34,7 +34,7 @@ module.exports = {
       '/admin': {
         // target: 'http://192.168.137.121:8080/admin',
         // target: 'http://192.168.1.205:8080/admin',
-        target: 'http://127.0.0.1:8080/admin',
+        target: 'http://192.168.1.202:8080/admin',
         changeOrigin: true,
         pathRewrite: {
           '^/admin': ''
