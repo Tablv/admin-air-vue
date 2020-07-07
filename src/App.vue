ca5afea6ec1dd7c172ca3f4705ba57237d4ef794
lvchangjie <1204479385@qq.comm>
2020-07-07 09:49:48 +0800

    滚动条样式

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,18 @@ export default {
   name: 'App'
 }
 </script>
+<style lang="scss">
+body {
+  ::-webkit-scrollbar {
+    width: 6px;
+    margin-left: 10px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #bfbfbf;
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #afafaf;
+  }
+}
+</style>
